ccd0cf8c48a84059ddbc9d76c02abf469f5722bd
Peter Lajos <937778+pmckl@users.noreply.github.com>
2022-11-10 23:49:58 +0100

    remove privates

--- test/second/main.tf
+++ test/second/main.tf
@@ -2,9 +2,6 @@ module "public" {
   source = "git@github.com:patrickjahns/terraform-module-public.git?ref=0.0.0"
 }
 
-module "private" {
-  source = "git@github.com:patrickjahns/terraform-module-private.git?ref=0.0.0"
-}
 
 module "registry_module" {
   source  = "vancluever/module/null"
